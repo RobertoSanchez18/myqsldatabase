10a1a7406181cb292656339313a239acf7f6dc6b
RobertoSanchez18 <roberto.sanchez@vallegrande.edu.pe>
2022-10-20 10:38:50 -0500

    avance

--- dbMinimarket.sql
+++ dbMinimarket.sql
@@ -1,2 +1,20 @@
 /*Listar base de datos del servidor*/
 SHOW DATABASES;
+
+/* Crear base de datos */
+CREATE DATABASE dbMinimarket;
+
+/* Poner en uso base de datos */
+USE dbMinimarket;
+
+
+/* Crear Tabla */
+CREATE TABLE VENDEDOR (
+    CODVEND  CHAR(6),
+    NOMVEND VARCHAR(50),
+    APEVEND VARCHAR(80),
+    DOCVEND VARCHAR(10),
+    CELVEND CHAR(9),
+    DOMVEND VARCHAR(80),
+    PRIMARY KEY (CODVEND)
+);
